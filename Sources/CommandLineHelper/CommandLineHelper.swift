d9affbd63c1ce5269b8f6a91ebfb779a257f7afc
Pere Calopa <pr.calopa@gmail.com>
2020-04-21 22:17:30 +0200

    add mandatory option arguments

--- Sources/CommandLineHelper/CommandLineHelper.swift
+++ Sources/CommandLineHelper/CommandLineHelper.swift
@@ -3,21 +3,37 @@ public struct CommandLineHelper {
 }
 
 public struct CommandLineOptions {
-  public static var options: [String] = []
+  public static var options: [(String, Bool)] = []
   
-  public static func addOption(_ name: String) {
-    options.append(name)
+  public static func addOption(_ name: String, mandatory: Bool = false) {
+    options.append((name, mandatory))
   }
 }
 
 public typealias Options = (String, String)
-public func parseArguments(_ args:[String]) -> [Options] {
+public func parseArguments(_ args:[String]) throws -> [Options] {
+  
+  let count = (args.count - 1) / 2
+  guard count != CommandLineOptions.options.count else {
+      throw CommandLineError.wrongArgumentsFound
+  }
+  
   var options: [Options] = []
-  for option in CommandLineOptions.options {
-    if let index = args.firstIndex(of: option) {
-      options.append((option, args[index+1]))
+  for (name, mandatory) in CommandLineOptions.options {
+    if let index = args.firstIndex(of: name) {
+      options.append((name, args[index+1]))
+    }
+    else {
+      if mandatory { throw CommandLineError.mandatoryArgumentNotFound }
     }
   }
   
+  
+  
   return options
+}
+
+public enum CommandLineError: Error {
+  case mandatoryArgumentNotFound
+  case wrongArgumentsFound
 }
