a7f8d5c2fe88fc9a41d7af99cb2da7daa70963bf
Pere Calopa <pr.calopa@gmail.com>
2020-04-21 22:18:20 +0200

    add test for argument parser

--- Tests/CommandLineHelperTests/CommandLineHelperTests.swift
+++ Tests/CommandLineHelperTests/CommandLineHelperTests.swift
@@ -2,14 +2,29 @@ import XCTest
 @testable import CommandLineHelper
 
 final class CommandLineHelperTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(CommandLineHelper().text, "Hello, World!")
+  
+  func testParseArguments() {
+    
+    CommandLineOptions.addOption("-test", mandatory: true)
+    CommandLineOptions.addOption("-notMandatory", mandatory: true)
+    
+    let input = ["executable", "-test", "arg1", "-unknown", "arg2"]
+    
+    do {
+      let result = try parseArguments(input)
+      XCTAssertEqual(result.count, 1)
+      XCTAssertEqual(result[0].1, "arg1")
+    } catch CommandLineError.mandatoryArgumentNotFound {
+      XCTAssert(true, "Mandatory argument not found")
+    } catch CommandLineError.wrongArgumentsFound {
+      XCTAssert(true, "Wrong arguments found")
+    } catch {
+      XCTFail()
     }
-
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+    
+  }
+  
+  static var allTests = [
+    ("testParseArguments", testParseArguments)
+  ]
 }
